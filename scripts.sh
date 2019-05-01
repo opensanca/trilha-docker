142cb3e862cc0b01e4318e30379d159b8b64be0e
Rafael Pereira Girolineto <rafapg.85@gmail.com>
2019-05-01 20:52:00 -0300

    Melhorias (Scripts e Dockerfile)

--- scripts.sh
+++ scripts.sh
@@ -2,7 +2,7 @@ exit 1;
 
 
 # Criar um container com MONGODB
-# Nome do container> c_mongo
+# Nome do container c_mongo (--name)
 # Mapeia a pasta mongodb do host como a pasta /data/db do container para salvar os dados mesmo que o contaier morra
 # Executa de forma independente (-d)
 docker run --name c_mongo -v $PWD/mongodb:/data/db --rm -d mongo
@@ -15,7 +15,7 @@ docker run --name c_mongo -v $PWD/mongodb:/data/db --rm -d mongo
 docker container run --rm  -it -v $PWD/node-rest-example:/usr/src/app -w /usr/src/app node npm install
 
 # Subir o servidor Node.js
-# Nome do container node_server
+# Nome do container node_server (--name)
 # Mapeia o código node (node-rest-example) para dentro do container (-v)
 # Define a pasta do código node como o diretório de trabalho (-w)
 # Executa de forma independente (-d)
@@ -41,3 +41,32 @@ curl -X GET http://localhost:8080/minicursos/$ID_MINICURSO
 # Remove minicurso com ID_MINICURSO
 ID_MINICURSO="5cca1daed766790010b11f8a"
 curl -X DELETE http://localhost:8080/minicursos/$ID_MINICURSO
+
+
+# Para o container antigo do Server Node (e apaga por causa da flag --rm)
+docker container stop node_server
+
+# Garante que o container foi apagado
+docker rm -f node_server
+
+# Subir o servidor Node.js
+# Nome do container node_server (--name)
+# Linka o container c_mongo (--link)
+# Mapeia o código node (node-rest-example) para dentro do container (-v)
+# Define a pasta do código node como o diretório de trabalho (-w)
+# Executa de forma independente (-d)
+# Mapeia a porta 8080 do host para a porta 3000 (padrão no nodejs) do container
+# Executa o comando `npm run start` dentro do container do `node`
+docker container run --rm --name node_server --link c_mongo -v $PWD/node-rest-example:/usr/src/app -w /usr/src/app -d -p 8080:3000 node npm run start
+
+
+docker build -t opensanca/minicurso-server:devconf19 node-rest-example/.
+
+
+# Subir o servidor Node.js
+# Nome do container node_server (--name)
+# Linka o container c_mongo (--link)
+# Executa de forma independente (-d)
+# Mapeia a porta 8080 do host para a porta 3000 (padrão no nodejs) do container
+# Inicia o container baseado na imagem com nosso código do server (opensanca/minicurso-server:devconf19)
+docker container run --rm --name node_server --link c_mongo -d -p 8080:3000 opensanca/minicurso-server:devconf19
